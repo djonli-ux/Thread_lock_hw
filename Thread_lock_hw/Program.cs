ae70124a80407f4e92d3cfaae2702b6eaea9c702
Jonatan Lifshits <djonli2003@gmail.com>
2023-08-27 15:56:10 +0300

    Thread lock testing

---
+++ Thread_lock_hw/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+int x = 0;
+object locker = new Object();
+
+for (int i = 1; i <= 5; ++i) 
+{
+    Thread t = new Thread(Print);
+    t.Name = $"thread_{i}";
+    t.Start();
+}
+void Print() 
+{
+    lock (locker) 
+    { 
+        x = 1;
+        for (int i = 1; i <= 10; ++i)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            x++;
+            Thread.Sleep(500);
+            }
+        }
+}
+
+
+
